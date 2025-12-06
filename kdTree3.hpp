4c0e189a6e1c957b876e5835a5e7aa8662bde61f
Michael24601 <msaba.lb@gmail.com>
2025-12-06 21:41:21 +0100

    Modifief kdTree for bulk insert

--- kdTree3.hpp
+++ kdTree3.hpp
@@ -2,9 +2,10 @@
 #ifndef KD_TREE_H
 #define KD_TREE_H
 
+#include <vector>
+#include <algorithm>
 #include "vector3.hpp"
 #include "point.hpp"
-#include <vector>
 
 // A 3D kd-tree implementation, used to store 3D points
 class KdTree3{
@@ -23,8 +24,10 @@ private:
 
     };
 
+
     typedef Node* NodePtr;
     NodePtr root;
+
 
     void destructorAux(NodePtr ptr){
         if(ptr){
@@ -33,6 +36,7 @@ private:
             delete ptr;
         }
     }
+
 
     void rangeQueryAux(const Vector3& center, real radius, 
         std::vector<Point>& points, NodePtr ptr, int axis) const {
@@ -57,6 +61,44 @@ private:
             rangeQueryAux(center, radius, points, 
                 ptr->right, (axis+1)%3);
         }
+    }
+
+
+    // Finds median point in given range (both sides inclusive), 
+    // inserts it into the tree, and then recurses on either side of it.
+    // The axis alternates per step.
+    NodePtr insertBulkAux(std::vector<Point>& points, int start, int end, 
+        int axis){
+
+        // The node at which this function was called must be
+        // a leaf if start > end.
+        if(start > end){
+            return nullptr;
+        }
+        
+        // We find the middle index
+        int middle = (start+end)/2;
+
+        // We then use this function, which sorts only the middle element
+        // of the array, essentially ensuring all elements to the
+        // left are smaller, and all elements to the right are larger
+        // (within the given range only, with respect to the current axis).
+        std::nth_element(
+            points.begin() + start,
+            points.begin() + middle,
+            points.begin() + end + 1,
+            [axis](const Point& p1, const Point& p2) { 
+                return p1.getPoint()[axis] < p2.getPoint()[axis]; 
+            }
+        );
+
+        // We then insert the middle element into the tree as a new node.
+        NodePtr node = new Node(points[middle]);
+        // We then recurse
+        node->left = insertBulkAux(points, start, middle-1, (axis+1) % 3);
+        node->right = insertBulkAux(points, middle+1, end, (axis+1) % 3);
+        
+        return node;
     }
 
 public:
@@ -109,6 +151,27 @@ public:
         std::vector<Point>& points) const {
 
         rangeQueryAux(center, radius, points, root, 0);
+    }
+
+
+    // Inserts bulk data all at once into the tree.
+    // Balanced kd-trees perform better, and since we have access
+    // to all the data at once, instead of a self-balancing tree,
+    // we can ensure balance by inserting the data in a specific way.
+    // We insert the median, then recursively search for the median
+    // in the two halves that we have left, alternating the axis
+    // we use to find the median.
+    // This also removes the need to use the insert function.
+    // Note that this assumes the tree is emtpy.
+    // Also note that the points vector will be modified (some of its
+    // elements will be sorted).
+    void insertBulk(std::vector<Point>& points){
+        if(root){
+            return;
+        }
+        else{
+            root = insertBulkAux(points, 0, points.size()-1, 0);
+        }
     }
 
 };
